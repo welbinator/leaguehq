440ee39ffce5fb0daaf73d4a4c66d05ab181a8bf
James Welbes <james.welbes@gmail.com>
2026-03-25 00:15:13 -0500

    fix: set tsconfig target ES2017 to prevent ES5 compat errors; fix Uint8Array buffer type

--- src/components/push/PushManager.tsx
+++ src/components/push/PushManager.tsx
@@ -29,7 +29,7 @@ export function PushManager() {
           }
           sub = await reg.pushManager.subscribe({
             userVisibleOnly: true,
-            applicationServerKey: urlBase64ToUint8Array(publicKey),
+            applicationServerKey: urlBase64ToUint8Array(publicKey).buffer,
           });
         }
 
